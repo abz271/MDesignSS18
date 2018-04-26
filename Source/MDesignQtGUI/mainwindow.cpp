d897955b661da09f9e09874b0271963439313451
Max Rostock <Max.Rostock@haw-hamburg.de>
2018-04-26 16:39:14 +0200

    MDesingQtGUI added

---
+++ Source/MDesignQtGUI/mainwindow.cpp
@@ -0,0 +1,65 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QDebug>
+#include <QtWidgets>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QPixmap gamefield("C:/MDesign/Spielfeld.png");
+    ui->gamefield_label->setPixmap(gamefield);
+    arduino = new QSerialPort;
+    arduino_is_available = false;
+    arduino_port_name = "";
+
+    qDebug() << "Number of available ports: " << QSerialPortInfo::availablePorts().length();
+    /*foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
+        qDebug() << "Has vendor ID: " << serialPortInfo.hasVendorIdentifier();
+        if(serialPortInfo.hasVendorIdentifier()){
+            qDebug() << "Vendor ID: " << serialPortInfo.vendorIdentifier();
+        }
+        qDebug() << "Has Product ID: " << serialPortInfo.hasProductIdentifier();
+        if(serialPortInfo.hasProductIdentifier()){
+            qDebug() << "Product ID: " << serialPortInfo.productIdentifier();
+        }
+    }*/
+    foreach(const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts()){
+        if(serialPortInfo.hasVendorIdentifier() && serialPortInfo.hasProductIdentifier()){
+            if(serialPortInfo.vendorIdentifier() == arduino_uno_vendor_id){
+                if(serialPortInfo.productIdentifier() == arduino_uno_product_id){
+                    arduino_port_name = serialPortInfo.portName();
+                    arduino_is_available = true;
+                }
+            }
+        }
+    }
+    if(arduino_is_available){
+        arduino->setPortName(arduino_port_name);
+        arduino->open(QSerialPort::ReadOnly);
+        arduino->setBaudRate(QSerialPort::Baud9600);
+        arduino->setDataBits(QSerialPort::Data8);
+        arduino->setParity(QSerialPort::NoParity);
+        arduino->setStopBits(QSerialPort::OneStop);
+        arduino->setFlowControl(QSerialPort::NoFlowControl);
+        QObject::connect(arduino, SIGNAL(readyRead()), this, SLOT(readDataFromArduino()));
+    }
+    else{
+        QMessageBox::warning(this, "Port error", "Couldn't find the Arduino!");
+    }
+}
+
+MainWindow::~MainWindow()
+{
+    if(arduino->isOpen()){
+        arduino->close();
+    }
+    delete ui;
+}
+
+void MainWindow::readDataFromArduino(){
+    qDebug() << "Serialport works!\n";
+}
---
+++ Source/MDesignQtGUI/mainwindow.h
@@ -0,0 +1,31 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QSerialPort>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+public slots:
+    void readDataFromArduino();
+
+private:
+    Ui::MainWindow *ui;
+    QSerialPort* arduino;
+    static const quint16 arduino_uno_vendor_id = 9025;
+    static const quint16 arduino_uno_product_id = 67;
+    QString arduino_port_name;
+    bool arduino_is_available;
+};
+
+#endif // MAINWINDOW_H
